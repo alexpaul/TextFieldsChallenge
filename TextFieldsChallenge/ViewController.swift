314c4466628258ae62b203b3d230f18800fdfd9a
Alex Paul <axp_alpaul@yahoo.com>
2015-06-20 13:12:53 -0400

    Complete challenge with all 3 solutions

--- TextFieldsChallenge/ViewController.swift
+++ TextFieldsChallenge/ViewController.swift
@@ -8,16 +8,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // IBOutlets 
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var cashTextField: UITextField!
+    @IBOutlet weak var lockableTextField: UITextField!
+    @IBOutlet weak var lockableSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+                
+        zipCodeTextField.delegate = self
+        cashTextField.delegate = self
+        lockableTextField.delegate = self
+        
+        // Initial text for the Cash Text Field $0.00
+        cashTextField.text = "$0.00"
+    }
+    
+    @IBAction func lockableSwithToggled(sender: UISwitch) {
+        // Dismiss the keyboard so lock will take effect
+        lockableTextField.resignFirstResponder()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: UITextFieldDelegate Methods
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        var dismissKeyboard = true
+        
+        if textField == lockableTextField {
+            dismissKeyboard = (lockableSwitch.on) ? true : false
+            return dismissKeyboard
+        }
+        return dismissKeyboard
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        var lengthOfZipCode = (zipCodeTextField.text as NSString).length
+        if lengthOfZipCode == 5{
+            zipCodeTextField.resignFirstResponder()
+        }
+        
+        if textField == cashTextField {
+            // Remove $ if it exist in cash text field
+            var modifiedString = (cashTextField.text as NSString).stringByReplacingOccurrencesOfString("$", withString: " ")
+            println(modifiedString)
+            
+            // Construct the text that will be changed
+            var newText: NSString = modifiedString // convert String to NSString
+            newText = newText.stringByReplacingCharactersInRange(range, withString: string)
+            
+            var newTextDoubleValue: Double = 0.0
+            
+            if cashTextField.text == "" { // first instance divide value by 100
+                newTextDoubleValue = newText.doubleValue / 100
+            }else{ // multiply by 10
+                newTextDoubleValue = newText.doubleValue * 10
+            }
+        
+            newText = "\(newTextDoubleValue)"
+            
+            cashTextField.text = "$\(newText)" // e.g $12.34
+            
+            return false
+        }
+        return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        // With the case where zip code length is 5
+        return true
     }
 
 
